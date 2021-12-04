6b1af3b2c13f4e9ce53721b18f27dd20af60b6d6
q3769 <wqt.net@gmail.com>
2021-12-04 14:38:21 -0600

    optional skip flag when normal version number is zero

--- src/main/java/wqt/maven/plugins/semver/NormalNumberIncrementer.java
+++ src/main/java/wqt/maven/plugins/semver/NormalNumberIncrementer.java
@@ -1,18 +1,14 @@
-/* 
+/*
  * The MIT License
- *
  * Copyright 2020 Qingtian Wang.
- *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,32 +20,45 @@
 package wqt.maven.plugins.semver;
 
 import com.github.zafarkhaja.semver.Version;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.maven.plugins.annotations.Parameter;
 
 /**
- *
  * @author Qingtian Wang
- *
- * Increment one of the three numbers in the normal release portion of the SemVer text
+ *         Increment one of the three numbers in the normal release portion of the SemVer text
  */
 public abstract class NormalNumberIncrementer extends Updater {
+
+    private static final Logger LOG = Logger.getLogger(NormalNumberIncrementer.class.getName());
 
     private static final String SNAPSHOT = "SNAPSHOT";
 
     /**
-     * Flag to append SNAPSHOT as the prerelease label in the target version. Expected to be passed in by user as a CLI
-     * -D parameter.
+     * Flag to append SNAPSHOT as the prerelease label in the target version. Expected to be passed in as a -D
+     * parameter from CLI.
      */
     @Parameter(property = "snapshot", defaultValue = "false", required = false)
     protected boolean snapshot;
 
     /**
-     *
+     * Flag to skip any change on the version number if the current number is zero (0). Expected to be passed in as a -D
+     * parameter from CLI. Default to false.
+     */
+    @Parameter(property = "skipOnZero", defaultValue = "false", required = false)
+    protected boolean skipOnZero;
+
+    /**
      * @param original from POM
      * @return result SemVer by incrementing one of the normal digits of the original version
      */
     @Override
     protected Version update(Version original) {
+        if (skipOnZero) {
+            LOG.log(Level.INFO, "Skipping SemVer increment as instructed by skip flag. Version unchanged: {0}", original
+                    .toString());
+            return original;
+        }
         Version incremented = incrementNormalNumber(original);
         if (!snapshot) {
             return incremented;
@@ -58,7 +67,6 @@ public abstract class NormalNumberIncrementer extends Updater {
     }
 
     /**
-     *
      * @param original the SemVer one of whose normal numbers is about to be incremented
      * @return target version to update POM
      */
