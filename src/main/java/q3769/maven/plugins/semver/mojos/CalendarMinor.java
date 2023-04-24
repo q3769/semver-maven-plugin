15f17019965daec367e7e8a5858ba16f62d5237f
q3769 <wqt.net@gmail.com>
2023-04-24 15:49:37 -0500

    + added feature: calendar-minor

---
+++ src/main/java/q3769/maven/plugins/semver/mojos/CalendarMinor.java
@@ -0,0 +1,61 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Qingtian Wang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package q3769.maven.plugins.semver.mojos;
+
+import com.github.zafarkhaja.semver.Version;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import q3769.maven.plugins.semver.Updater;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Increments minor version to current calendar date in basic ISO format. If the resulting version is newer than the
+ * original POM version, up the POM version to the new one. Otherwise, errors out.
+ *
+ * @author Qingtian Wang
+ */
+@Mojo(name = "calendar-minor", defaultPhase = LifecyclePhase.NONE)
+public class CalendarMinor extends Updater {
+
+    /**
+     * @param original
+     *         POM project version whose major number is to be incremented
+     * @return New semver version whose major number is incremented to current date in basic ISO format. Error out
+     */
+    @Override
+    protected Version update(Version original) throws MojoFailureException {
+        Version newVersion = Version.forIntegers(original.getMajorVersion(),
+                original.getMinorVersion(),
+                Integer.parseInt(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE)));
+        if (original.equals(newVersion) || original.greaterThan(newVersion)) {
+            throw new MojoFailureException(
+                    "Original POM version: " + original + " is already the same as or newer than the intended update: "
+                            + newVersion);
+        }
+        return newVersion;
+    }
+}
---
+++ src/test/java/q3769/maven/plugins/semver/mojos/CalMinorTest.java
@@ -0,0 +1,63 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Qingtian Wang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package q3769.maven.plugins.semver.mojos;
+
+import com.github.zafarkhaja.semver.Version;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Qingtian Wang
+ */
+class CalMinorTest {
+
+    private static final String TODAY = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+    private static final Version EXPECTED_RESULT = new Version.Builder("1.1." + TODAY).build();
+
+    private final CalendarMinor instance = new CalendarMinor();
+
+    @Test
+    void testShouldErrorOutIfOriginalMajorVersionIsHigher() {
+        final int someLaterDay = Integer.parseInt(TODAY) + 10000;
+        Version original = new Version.Builder("1.1." + someLaterDay).build();
+
+        Assertions.assertThrows(MojoFailureException.class, () -> instance.update(original));
+    }
+
+    @Test
+    void testShouldIncrementMajorToToday() throws MojoFailureException {
+        final int someDayEarlier = Integer.parseInt(TODAY) - 10000;
+        Version original = new Version.Builder("1.1." + someDayEarlier).build();
+
+        Version result = instance.update(original);
+
+        assertEquals(EXPECTED_RESULT, result);
+    }
+}
